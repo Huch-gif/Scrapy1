8827320fd73b0368fcbe33396dd64d20dcf07fdb
Huch-gif <savvabluz2@gmail.com>
2025-06-13 19:15:48 +0300

    add new code(0

--- rona_lighting/rona_lighting/settings.py
+++ rona_lighting/rona_lighting/settings.py
@@ -1,3 +1,6 @@
 BOT_NAME = 'rona_lighting'
 SPIDER_MODULES = ['rona_lighting.spiders']
 NEWSPIDER_MODULE = 'rona_lighting.spiders'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+DOWNLOAD_DELAY = 2  # Задержка 2 сек между запросами
+ROBOTSTXT_OBEY = False  # Игнорировать robots.txt
--- rona_lighting/rona_lighting/spiders/rona_lights.py
+++ rona_lighting/rona_lighting/spiders/rona_lights.py
@@ -1,14 +1,28 @@
+
 import scrapy
 
 class RonaLightsSpider(scrapy.Spider):
     name = "rona_lights"
     allowed_domains = ["rona-servis.ru"]
-    start_urls = ["https://rona-servis.ru/catalog/svet/"]
+    start_urls = ["https://www.rona-servis.ru/catalog"]
 
     def parse(self, response):
-        for product in response.css('div.product-item'):
+        # Шаг 1: Парсим ссылки на категории освещения
+        categories = response.css('.collection-preview__link::attr(href)').getall()
+        for category in categories:
+            yield response.follow(category, callback=self.parse_category)
+
+    def parse_category(self, response):
+        # Шаг 2: Парсим товары на странице категории
+        products = response.css('.product-preview')
+        for product in products:
             yield {
-                'name': product.css('a::text').get().strip(),
-                'price': product.css('span.price::text').get().replace(' ', ''),
+                'name': product.css('.product-preview__description::text').get(),
+                'price': product.css('.price-current::text').get(),
                 'url': response.urljoin(product.css('a::attr(href)').get())
             }
+
+        # Шаг 3: Переход на следующую страницу (если есть)
+        next_page = response.css('.next::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse_category)
